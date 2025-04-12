8f6e5945950e6846080358a92a4550a791ed27b1
Khoa Cao <dkhoacao2906@gmail.com>
2025-04-12 10:58:22 +0700

    Update

--- main.py
+++ main.py
@@ -9,6 +9,10 @@ from ui.trial_page import display_trial_page
 
 load_dotenv()
 
+for dir_name in ["docx_output", "pdf_output"]:
+    path = os.path.join(os.path.dirname(__file__), dir_name)
+    os.makedirs(path, exist_ok=True)
+
 if "cleanup_started" not in st.session_state:
     cleanup_expired()
     st.session_state["cleanup_started"] = True
